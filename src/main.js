69d60b731c6021b2bc7c125638288a506b27b9ad
Fulano2141 <macuto2142dt@gmail.com>
2021-05-13 19:11:57 -0400

    auth init

--- src/main.js
+++ src/main.js
@@ -6,9 +6,21 @@ import "@mdi/font/css/materialdesignicons.css";
 import router from "./router";
 import store from "./store/store.js";
 import axios from "axios";
+import { Auth0Plugin } from "./auth";
+import { domain, clientId } from "../auth_config.json";
 
 Vue.config.productionTip = false;
-
+Vue.use(Auth0Plugin, {
+  domain,
+  clientId,
+  onRedirectCallback: (appState) => {
+    router.push(
+      appState && appState.targetUrl
+        ? appState.targetUrl
+        : window.location.pathname
+    );
+  },
+});
 Vue.filter("roundPercentage", function(value) {
   if (!value) return "0%";
   value = value.toFixed(2).toString();
